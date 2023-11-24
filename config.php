d73c1062d66ac10586520336b5b15d5a1599b617
Akira0202l <151883246+Akira0202l@users.noreply.github.com>
2023-11-23 23:44:58 -0300

    teste site

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+$dbHost = 'Localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'Cadastro';
+
+$conexao = new mysqli ($dbHost, $dbUsername, $dbPassword, $dbName);
+
+if ($conexao->connect_error)
+
+{ 
+echo "Erro";
+}
+
+
+
+?>
